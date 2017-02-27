a0862dcf16a5b9760d04d61704d52e23382dcd07
Lanvige Jiang <lanvige@gmail.com>
2017-02-27 17:49:38 +0800

    update docker-deploy config

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,5 @@ FROM lanvige/node-deploy:7.5.0-onbuild
 
 MAINTAINER Lanvige Jiang
 
-
 #ENTRYPOINT ["pm2", "start", "--no-daemon", "/app/bin/api"]
 # CMD []
--- shipitfile.js
+++ shipitfile.js
@@ -1,3 +1,4 @@
+var chalk = require('chalk');
 
 module.exports = function (shipit) {
   require('shipit-deploy')(shipit);
@@ -10,7 +11,7 @@ module.exports = function (shipit) {
       deployTo: '/data/repos/app-koa2bp',
       repositoryUrl: 'git@github.com:lanvige/koa2-boilerplate.git',
       branch: 'master',
-      ignores: ['.git', 'node_modules'],
+      ignores: ['.DS_Store', '.git', 'node_modules'],
       rsync: ['--del'],
       keepReleases: 2,
       // key: '/Users/lanvige/.ssh/id_rsa.pub',
@@ -32,23 +33,36 @@ module.exports = function (shipit) {
     return shipit.remote('pwd');
   });
 
-
-  shipit.blTask('build1', function () {
-    // ... 
+  // npm install & build
+  shipit.blTask('npm-install', function () {
     shipit.log('yarn install.');
-    shipit.local('pwd');
     return shipit.local('yarn install', { cwd: '/tmp/app-koa2boilerplate' })
   });
 
-  shipit.blTask('build2', function () {
-    // ... 
-    shipit.log('npm installed and build start.');
-    shipit.log('npm builed.');
+  shipit.blTask('npm-build', function () {
+    shipit.log('npm build start.');
     return shipit.local('npm run build', { cwd: '/tmp/app-koa2boilerplate' })
   });
 
   shipit.on('fetched', function () {
     console.log('itis npm build');
-    shipit.start(['build1', 'build2']);
+    shipit.start(['npm-install', 'npm-build']);
   });
+
+  // Docker build & run on Remote
+  shipit.blTask('docker-build', function () {
+    shipit.log(chalk.green('docker-compose build...'));
+    // return shipit.remote('cd '+shipit.releasePath+'; pwd');
+    return shipit.remote('cd /data/repos/app-koa2bp/current; docker-compose build');
+  });
+
+  shipit.blTask('docker-run', function () {
+    shipit.log(chalk.green('docker-compose up start...'));
+    return shipit.remote('cd /data/repos/app-koa2bp/current; docker-compose up')
+  });
+
+  shipit.on('deployed', function() {
+    shipit.log(chalk.green('Docker deploy start'));
+    shipit.start(['docker-build', 'docker-run']);
+  })
 };
